11388108e361f50caa72719115b2e46bb5426d37
twigform <twigscoolemail@gmail.com>
2026-03-05 14:54:12 -0500

    make everything use style vars and improve spacing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
+    if std::env::var("WAYLAND_DISPLAY").is_ok() {
+        std::env::set_var("QT_QPA_PLATFORM", "xcb");
+    }
     let main_window = MainWindow::new()?;
 
     main_window.run()
