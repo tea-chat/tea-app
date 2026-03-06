3e6efe0fb1db74412563c6aadd957d4d846cd917
twigform <twigscoolemail@gmail.com>
2026-03-06 14:02:15 -0500

    very WIP addition of placeholder elements in the userPanel

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+use std::{collections::HashMap, path::PathBuf};
+
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+  let library = HashMap::from([(
+    "lucide".to_string(),
+    PathBuf::from(lucide_slint::lib()),
+  )]);
+  let config = slint_build::CompilerConfiguration::new().with_library_paths(library);
+
+  slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
 }
